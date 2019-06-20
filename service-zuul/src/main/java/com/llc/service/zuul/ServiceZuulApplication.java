dc8c84e00f45c81a2345fb0dc06922712f60e222
llc <787254039@qq.com>
2019-06-20 14:50:57 +0800

    修改zuul pom坐标

--- service-zuul/src/main/java/com/llc/service/zuul/ServiceZuulApplication.java
+++ service-zuul/src/main/java/com/llc/service/zuul/ServiceZuulApplication.java
@@ -2,13 +2,12 @@ package com.llc.service.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableZuulProxy
-@EnableDiscoveryClient
+@EnableEurekaClient
 public class ServiceZuulApplication {
 
     public static void main(String[] args) {
