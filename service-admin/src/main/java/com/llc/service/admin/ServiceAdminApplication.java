2670b2740006d9d0fa58e0bc21b51240fbf80fbd
llc <787254039@qq.com>
2019-06-21 10:52:35 +0800

    修改pom坐标

--- service-admin/src/main/java/com/llc/service/admin/ServiceAdminApplication.java
+++ service-admin/src/main/java/com/llc/service/admin/ServiceAdminApplication.java
@@ -1,21 +1,19 @@
 package com.llc.service.admin;
 
-import com.netflix.discovery.EurekaNamespace;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Configuration;
 
 /**
  * 监控服务上下线
  */
 
+
 @Configuration
 @EnableAutoConfiguration
 @EnableAdminServer
-@EnableEurekaClient
 public class ServiceAdminApplication {
 
     public static void main(String[] args) {
